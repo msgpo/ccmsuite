2e2790dd7159e0cf1300e792ea36fa122074be32
Michael Hansen <hansen.mike@gmail.com>
2013-10-25 17:16:53 -0400

    Added all packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     author='tcstewar',
     author_email='tcstewar@github.com',
-    packages=['ccm'],
+    packages=['ccm', 'ccm.display', 'ccm.display.cairo', 'ccm.display.pygame', 'ccm.display.tk', 'ccm.legacy', 'ccm.lib', 'ccm.lib.actr', 'ccm.lib.nef', 'ccm.ui', 'ccm.tests'],
     url='https://github.com/tcstewar',
     license='LICENSE.txt',
     description='Python cognitive modeling suite',
