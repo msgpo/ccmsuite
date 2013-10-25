b1c0db76074c00233cb1a3a64f0a27287cbe3396
Michael Hansen <hansen.mike@gmail.com>
2013-10-25 17:11:23 -0400

    Added pip stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='ccm',
+    version='1.0.0',
+    author='tcstewar',
+    author_email='tcstewar@github.com',
+    packages=['ccm'],
+    url='https://github.com/tcstewar',
+    license='LICENSE.txt',
+    description='Python cognitive modeling suite',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
